09ef3403980f69c503e8751b84559639e1f3fbb5
Sarm-88 <diesel1840@gmail.com>
2023-08-03 11:40:38 +0100

    Colors

--- randColor.js
+++ randColor.js
@@ -1,5 +1,19 @@
 var divs = document.getElementsByTagName("div");
 
+var bodyStyles = window.getComputedStyle(document.body);
+var dark = bodyStyles.getPropertyValue("--dark");
+var primary = bodyStyles.getPropertyValue("--primary");
+var secondary = bodyStyles.getPropertyValue("--secondary");
+var light = bodyStyles.getPropertyValue("--light");
+
+var colors = [dark, primary, secondary, light]
+
 for (var i = 0; i < divs.length; i++) {
-    divs[i].style.backgroundColor = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
+    divs[i].style.backgroundColor = colors[rand_color()];
+}
+
+// "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")"
+
+function rand_color() {
+    return Math.floor(Math.random() * colors.length);
 }
