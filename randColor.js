b4822b49e0d53208de48660699a7fdbc0f7c62f2
Sarm <diesel1840@gmail.com>
2023-08-03 21:11:11 +0100

    Colors and Fonts

--- randColor.js
+++ randColor.js
@@ -12,8 +12,6 @@ for (var i = 0; i < divs.length; i++) {
     divs[i].style.backgroundColor = colors[rand_color()];
 }
 
-// "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")"
-
 function rand_color() {
     return Math.floor(Math.random() * colors.length);
 }
